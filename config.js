03b9a4e2a4f267de18a89a1a82eeb6bcad126fe7
Shreshth Agrawal <shreshth6002a@gmail.com>
2026-04-01 17:28:56 +0530

    hllooo

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 console.log(Hello,world);
 
-const API_KEY1 = "api_key_123456789asdfghj";
+const API_KEY = "api_key_123456789abcdef123456";
+const API_TOKEN = "token_abcdef1234567890abcdef";
+const SECRET_KEY = "secret_123456789abcdef12345";
